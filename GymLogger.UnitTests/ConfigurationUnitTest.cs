ed607a3914b7f8e77518e025f1b8eac2590ae8b3
David P <prozajik@outlook.com>
2022-10-16 19:55:25 +0200

    Fixed unit-tests -- TODO: Learn about fixture and integration testing

--- GymLogger.UnitTests/ConfigurationUnitTest.cs
+++ GymLogger.UnitTests/ConfigurationUnitTest.cs
@@ -1,18 +1,41 @@
 using GymLogger.API.Controllers;
+using GymLogger.API.Handler;
+using GymLogger.Context;
+using GymLogger.Shared.Models;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace GymLogger.UnitTests;
 
 public class ConfigurationUnitTest
 {
-    [Fact]
-    public void ConfigurationGet_ShouldReturnTrue()
+    public void InitContext(GymLoggerContext context)
     {
-        var mediatorMock = new Mock<IMediator>();
-        //var mock = new ConfigurationHandlerMock();
-        var controller = new ConfigurationController2(mediatorMock.Object);
-        var response = controller.GetConfigurations();
-        //Assert.Equal(4, )
+        var list = new List<Configuration>()
+            {
+                new Configuration() { Id = 1 },
+                new Configuration() { Id = 2 },
+                new Configuration() { Id = 5 }
+            };
+
+        context.Configurations.AddRange(list);
+        context.SaveChanges();
+    }
+
+    [Fact]
+    public async void ConfigurationGet_ShouldReturnTrue()
+    {
+        var mediator = new Mock<IMediator>();
+        var optionsBuilder = new DbContextOptionsBuilder<GymLoggerContext>();
+        optionsBuilder.UseInMemoryDatabase("GymLoggerContext");
+        var context = new GymLoggerContext(optionsBuilder.Options);
+        InitContext(context);
+
+        var command = new GetConfigurationsQuery();
+        var handler = new ConfigurationHandler(context);
+        var res = await handler.Handle(command, new CancellationToken());
+        Assert.True(res.Count == 3);
+        
     }
 }
